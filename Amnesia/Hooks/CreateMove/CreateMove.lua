04dad04590e92a51ac4330ca55ac1884e19aea1a
astra001 <theastra001@gmail.com>
2016-08-04 13:08:51 +0300

    Update CreateMove.lua

--- Amnesia/Hooks/CreateMove/CreateMove.lua
+++ Amnesia/Hooks/CreateMove/CreateMove.lua
@@ -1,4 +1,4 @@
---[[ Using GAMEMODE:#HOOKNAME method is better, because some anti-cheats can detect hook.Add feature. ]]--
+--[[ Using GAMEMODE:#HOOKNAME method is better, because some anti-cheats can detect hook.Add function. ]]--
 
 function GAMEMODE:CreateMove(ucmd)
 	MsgC(Color(114, 137, 208), "[CreateMove] Hello World!", "\n");
